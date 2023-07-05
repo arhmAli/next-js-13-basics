9a175684e3b06506f6eda29fa40ec29cbec516f2
Arham Khan <arhamkhank353@gmail.com>
2023-07-06 01:09:20 +0500

    contact us page

---
+++ simple-routing-practice/project/app/contact/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function ContactPage() {
+  return (
+    <div>
+        <h1>
+            Welcome to the contact us page
+        <Link href="/">
+        Home
+        </Link>
+        </h1>
+    </div>
+  )
+}
