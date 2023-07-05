c3eff9f8a38ad9b19007225ac9a4385ded435d64
Arham Khan <arhamkhank353@gmail.com>
2023-07-06 01:09:35 +0500

    about page

---
+++ simple-routing-practice/project/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function AboutPage() {
+  return (
+    <div>
+        <h1>Welcome to the about page</h1>
+        <Link href="/">
+        Home
+        </Link>
+    </div>
+  )
+}
