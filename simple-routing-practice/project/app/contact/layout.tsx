6bb030cff25ed199963e0a62bbcaa69a63ddc0a4
Arham Khan <arhamkhank353@gmail.com>
2023-07-07 02:06:25 +0500

    tried nested layout

---
+++ simple-routing-practice/project/app/contact/layout.tsx
@@ -0,0 +1,10 @@
+export default function ContactLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return <section>
+        <h1>From contact layout</h1>
+        {children}
+        </section>
+  }
